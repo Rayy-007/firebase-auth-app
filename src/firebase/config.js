ca935f9950ddc1fc6afc137fb0bda0ee79d98b59
Rayy-007 <minkhant88887@gmail.com>
2024-02-28 10:32:52 +0630

    initialize cloude firebase database

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDdE9Ew1wLuiXjooydxEtmb-fagvxFykPE",
@@ -10,4 +11,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-export { app, auth };
+const db = getFirestore(app);
+export { app, auth, db };
