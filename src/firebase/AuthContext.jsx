6458aefd6e9cfb7b2387e92a35bfc15ff05f6a59
Rayy-007 <minkhant88887@gmail.com>
2024-03-20 16:09:39 +0630

    feat: seperate the file and logic

---
+++ src/firebase/AuthContext.jsx
@@ -0,0 +1,94 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { auth } from "./config";
+import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext();
+
+export const useFirebaseAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
+
+  const provider = new GoogleAuthProvider();
+
+  const [signedInUser, setSignedInUser] = useState();
+
+  // Checking if the user is signed in or not
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setSignedInUser(user.auth.currentUser);
+        navigate("/success");
+      } else {
+        navigate("/");
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  // Sign Up with Email
+  const signUpWithEmail = (signUpData) => {
+    createUserWithEmailAndPassword(auth, signUpData.email, signUpData.password)
+      .then((userInfo) => {
+        alert("Created account successfully");
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+
+  // Sign In with Email
+  const signInWithEmail = (signInData) => {
+    signInWithEmailAndPassword(auth, signInData.email, signInData.password)
+      .then((userInfo) => {
+        alert("Successfully signed in");
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+
+  // Sign In & Up with Google
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then(() => {
+        alert("Signed In with Google");
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+
+  // Sign Out
+  const signOutHandle = () => {
+    signOut(auth)
+      .then(() => {
+        alert("Sign out successfully!");
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signedInUser,
+        signInWithEmail,
+        signUpWithEmail,
+        signInWithGoogle,
+        signOutHandle,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ src/firebase/FetchContext.jsx
@@ -0,0 +1,57 @@
+import { useContext, createContext, useState, useEffect } from "react";
+import { FetchOnceData, FetchRealTimeData } from "./FetchData";
+import { AuthContext } from "../App";
+
+const FetchContext = createContext();
+
+export const useFetchData = () => useContext(FetchContext);
+
+export const FetchDataProvider = ({ children }) => {
+  const { signedInUser } = useContext(AuthContext);
+
+  const [postsData, setPostsData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  // Real time fetching data from Firebase
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchRealTimeData = async () => {
+      try {
+        FetchRealTimeData(signedInUser?.uid, setPostsData);
+        setIsLoading(false);
+        setErrorMessage(null);
+      } catch (error) {
+        setIsLoading(false);
+        setErrorMessage(
+          "Opps! Something went wrong! Please refresh Post Lists!"
+        );
+      }
+    };
+    fetchRealTimeData();
+  }, [signedInUser]);
+
+  // Manually Refresh the Fetch Data
+  const refreshPostsData = async () => {
+    setIsLoading(true);
+    try {
+      const data = await FetchOnceData(signedInUser?.uid);
+      setPostsData(data);
+      setIsLoading(false);
+      setErrorMessage(null);
+    } catch (error) {
+      setIsLoading(false);
+      setErrorMessage("Opps! Something went wrong! Please refresh Post Lists!");
+    }
+  };
+
+  return (
+    <FetchContext.Provider
+      value={{ refreshPostsData, postsData, isLoading, errorMessage }}
+    >
+      {children}
+    </FetchContext.Provider>
+  );
+};
+
+export default FetchContext;
